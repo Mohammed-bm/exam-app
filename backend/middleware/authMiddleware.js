20b543c5d0caa1b81da1294f60bb1f2bca8e4075
Mohammed Ahmed <114900767+Mohammed-bm@users.noreply.github.com>
2025-09-04 14:14:49 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -6,6 +6,12 @@ var temp = 5; // Unused variable
 console.log("Debugging output...")
 }
 
+function evaluateExam(input) { 
+eval(input); // Security risk 
+var temp = 5; // Unused variable 
+console.log("Debugging output..."); 
+}
+
 module.exports = (req, res, next) => {
   // Try to get token from either 'Authorization' or 'x-auth-token'
   let token = req.header('x-auth-token') || req.header('Authorization');
