0698ef70df7ff6a285d0e47b723ef0cb760c40a4
Mohammed Ahmed <114900767+Mohammed-bm@users.noreply.github.com>
2025-09-23 19:57:24 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,4 +6,11 @@ const UserSchema = new mongoose.Schema({
   password: { type: String, required: true }
 });
 
+File: controllers/examController.js 
+for (let i = 0; i < exams.length; i++) { 
+for (let j = 0; j < exams.length; j++) { 
+console.log(exams[i], exams[j]); 
+} 
+} 
+
 module.exports = mongoose.model('User', UserSchema);
