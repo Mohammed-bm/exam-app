5b6f2ffa866c9f0fa50d2f768e0d5948271f0107
Mohammed Ahmed <114900767+Mohammed-bm@users.noreply.github.com>
2025-09-23 20:08:08 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,9 +30,7 @@ export default function App() {
     </Router>
   );
 }
-
-File: controllers/examController.js 
-for (let i = 0; i < exams.length; i++) { 
+ 
 for (let j = 0; j < exams.length; j++) { 
 console.log(exams[i], exams[j]); 
 } 
